4bacb82fa82baf3f0e1fec16ffc2077fae42408a
Nahuel Batallan <NAhuelBAtallanDOta@gmail.com>
2023-11-02 20:20:36 -0300

    Correccion grupales

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require(`path`);
 
 
 
-const publicPath = path.resolve(__dirname, '/public')
+const publicPath = path.resolve(__dirname, './public')
 app.use( express.static(publicPath) );
 
 app.listen(3030, () => {
